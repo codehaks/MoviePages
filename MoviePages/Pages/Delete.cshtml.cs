f9c30ef8d2c73142396aaeded12ebeea9b587656
Hakim <ghods.hakim@hotmail.com>
2024-09-28 17:23:54 +0330

    edit

---
+++ MoviePages/Pages/Delete.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MoviePages.Data;
+
+namespace MoviePages.Pages
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly MoviePages.Data.MovieDbContext _context;
+
+        public DeleteModel(MoviePages.Data.MovieDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (movie is not null)
+            {
+                Movie = movie;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie != null)
+            {
+                Movie = movie;
+                _context.Movies.Remove(Movie);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
