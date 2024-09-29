163a8bdeaca742e416c7322d2fa75ebb949a7b41
Hakim <ghods.hakim@hotmail.com>
2024-09-29 16:43:21 +0330

    Update db

--- MoviePages/Data/MovieDbContext.cs
+++ MoviePages/Data/MovieDbContext.cs
@@ -9,7 +9,7 @@ public class MovieDbContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data source=movies.db");
-       // base.OnConfiguring(optionsBuilder);
+        // base.OnConfiguring(optionsBuilder);
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -24,7 +24,28 @@ public class MovieDbContext : DbContext
             new Movie { Id = 7, Name = "Inception", Year = 2010 },
             new Movie { Id = 8, Name = "Fight Club", Year = 1999 },
             new Movie { Id = 9, Name = "The Matrix", Year = 1999 },
-            new Movie { Id = 10, Name = "The Lord of the Rings: The Return of the King", Year = 2003 }
+            new Movie { Id = 10, Name = "The Lord of the Rings: The Return of the King", Year = 2003 },
+            // Adding 20 more movies
+            new Movie { Id = 11, Name = "Gladiator", Year = 2000 },
+            new Movie { Id = 12, Name = "The Lion King", Year = 1994 },
+            new Movie { Id = 13, Name = "Schindler's List", Year = 1993 },
+            new Movie { Id = 14, Name = "Goodfellas", Year = 1990 },
+            new Movie { Id = 15, Name = "Star Wars: Episode IV - A New Hope", Year = 1977 },
+            new Movie { Id = 16, Name = "Avengers: Endgame", Year = 2019 },
+            new Movie { Id = 17, Name = "Saving Private Ryan", Year = 1998 },
+            new Movie { Id = 18, Name = "The Silence of the Lambs", Year = 1991 },
+            new Movie { Id = 19, Name = "Jurassic Park", Year = 1993 },
+            new Movie { Id = 20, Name = "The Departed", Year = 2006 },
+            new Movie { Id = 21, Name = "The Prestige", Year = 2006 },
+            new Movie { Id = 22, Name = "Memento", Year = 2000 },
+            new Movie { Id = 23, Name = "The Green Mile", Year = 1999 },
+            new Movie { Id = 24, Name = "Braveheart", Year = 1995 },
+            new Movie { Id = 25, Name = "American Beauty", Year = 1999 },
+            new Movie { Id = 26, Name = "The Usual Suspects", Year = 1995 },
+            new Movie { Id = 27, Name = "The Avengers", Year = 2012 },
+            new Movie { Id = 28, Name = "The Wolf of Wall Street", Year = 2013 },
+            new Movie { Id = 29, Name = "Django Unchained", Year = 2012 },
+            new Movie { Id = 30, Name = "Interstellar", Year = 2014 }
         );
     }
 
---
+++ MoviePages/Migrations/20240929131132_addmoremovies.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MoviePages.Migrations
+{
+    /// <inheritdoc />
+    public partial class addmoremovies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "Id", "Name", "Year" },
+                values: new object[,]
+                {
+                    { 11, "Gladiator", 2000 },
+                    { 12, "The Lion King", 1994 },
+                    { 13, "Schindler's List", 1993 },
+                    { 14, "Goodfellas", 1990 },
+                    { 15, "Star Wars: Episode IV - A New Hope", 1977 },
+                    { 16, "Avengers: Endgame", 2019 },
+                    { 17, "Saving Private Ryan", 1998 },
+                    { 18, "The Silence of the Lambs", 1991 },
+                    { 19, "Jurassic Park", 1993 },
+                    { 20, "The Departed", 2006 },
+                    { 21, "The Prestige", 2006 },
+                    { 22, "Memento", 2000 },
+                    { 23, "The Green Mile", 1999 },
+                    { 24, "Braveheart", 1995 },
+                    { 25, "American Beauty", 1999 },
+                    { 26, "The Usual Suspects", 1995 },
+                    { 27, "The Avengers", 2012 },
+                    { 28, "The Wolf of Wall Street", 2013 },
+                    { 29, "Django Unchained", 2012 },
+                    { 30, "Interstellar", 2014 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 25);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 26);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 27);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 28);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 29);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "Id",
+                keyValue: 30);
+        }
+    }
+}
