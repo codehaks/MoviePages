cf293002167dbe69ecb51ac81fed62d91b7e4b0b
Hakim <ghods.hakim@hotmail.com>
2024-10-05 10:01:57 +0330

    setups

--- MoviePages/Program.cs
+++ MoviePages/Program.cs
@@ -3,9 +3,30 @@ using MoviePages.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<MovieDbContext>();
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages()
+    .AddViewLocalization(Microsoft.AspNetCore.Mvc.Razor.LanguageViewLocationExpanderFormat.Suffix)
+    .AddDataAnnotationsLocalization();
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[] { "fa", "en" };
+    options.DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("fa");
+    options.SupportedCultures = supportedCultures.Select(c => new System.Globalization.CultureInfo(c)).ToList();
+    options.SupportedUICultures = supportedCultures.Select(c => new System.Globalization.CultureInfo(c)).ToList();
+});
 
 var app = builder.Build();
+
+// Localization middleware to enable request localization
+var supportedCultures = new[] { "fa", "en" };
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture("fa")
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
 
 //app.UseStaticFiles();
 app.MapStaticAssets();
